d0294d16bfa22e2075cfd7bd31fb8a45b22b9e1e
Koushik-007 <2015-516-802@student.du.ac.bd>
2023-06-02 19:58:16 +0600

    Add static pages and themes

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "about.html"));
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
 });
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "contact.html"));
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
 });
