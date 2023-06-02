7aa0f918f5f4c47f0443de5c75fdfa717ea9116a
Koushik-007 <2015-516-802@student.du.ac.bd>
2023-06-02 23:08:46 +0600

    Add EJS for dynamic rendering

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const ejs = require("ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.listen(3000, () => {
@@ -8,14 +11,18 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  // res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  // res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  // res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
 });
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  // res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
 });
